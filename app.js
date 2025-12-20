4fba9a6b865392d196b33691f4ca319ea5b30afe
olubiyosamuel2007 <olubiyosamuel08@gmail.com>
2025-12-20 10:26:10 +0100

    Update and rename group.html to app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const tabs = document.querySelectorAll(".tabs button");
+
+tabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+    tabs.forEach(t => t.classList.remove("active"));
+    tab.classList.add("active");
+  });
+});
